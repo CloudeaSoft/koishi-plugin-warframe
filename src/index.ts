554821e07fe8987185776ab6eaa5672d96eb5ae2
Cloudea <cloudeasoft@qq.com>
2025-12-05 17:14:14 +0800

    refactor: optimize code structure of index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,7 @@
 import { Context, Schema } from "koishi";
 
-import {
-  arbitrationCommand,
-  fissureCommand,
-  steelPathFissureCommand,
-  aboutCommand,
-  wmCommand,
-  wmrCommand,
-  railjackFissureCommand,
-  circuitCommand,
-} from "./commands";
-import { onReadyHandler } from "./hooks/on-ready";
+import * as commands from "./commands";
+import * as hooks from "./hooks/on-ready";
 
 export const name = "warframe";
 
@@ -23,6 +14,11 @@ export const Config: Schema<Config> = Schema.object({
 });
 
 export function apply(ctx: Context) {
+  setupHooks(ctx);
+  setupCommands(ctx);
+}
+
+const setupHooks = (ctx: Context) => {
   ctx.on("message", (session) => {
     if (ctx.config.developerMode) {
       ctx.logger.info(`
@@ -31,25 +27,27 @@ export function apply(ctx: Context) {
         用户: ${session.author.name}`);
     }
   });
-  ctx.on("ready", onReadyHandler);
+  ctx.on("ready", hooks.onReadyHandler);
+};
 
+const setupCommands = (ctx: Context) => {
   ctx.command("wm <itemId:text>", "请用wmi替代").action((a, b) => {
     if (ctx.config.developerMode) {
       ctx.logger.info(`WFM Plugin received command wm: '${b}'`);
     }
-    return wmCommand(a, b);
+    return commands.wmCommand(a, b);
   });
   ctx.command("wmr <itemId:text>", "查询wm的紫卡价格").action((a, b) => {
     if (ctx.config.developerMode) {
       ctx.logger.info(`WFM Plugin received command wmr: '${b}'`);
     }
-    return wmrCommand(a, b);
+    return commands.wmrCommand(a, b);
   });
   ctx.command("wmi <msg:text>", "查询wm的物品价格").action((a, b) => {
     if (ctx.config.developerMode) {
       ctx.logger.info(`WFM Plugin received command wmi: '${b}'`);
     }
-    return wmCommand(a, b);
+    return commands.wmCommand(a, b);
   });
 
   ctx
@@ -57,34 +55,34 @@ export function apply(ctx: Context) {
     .alias("arbi")
     .alias("仲裁")
     .alias("仲裁表")
-    .action(arbitrationCommand);
+    .action(commands.arbitrationCommand);
   ctx
     .command("fissure", "当前虚空裂隙")
     .alias("裂缝")
     .alias("裂隙")
-    .action(fissureCommand);
+    .action(commands.fissureCommand);
   ctx
     .command("spfissure", "当前钢铁之路虚空裂隙")
     .alias("钢铁裂缝")
     .alias("钢铁裂隙")
-    .action(steelPathFissureCommand);
+    .action(commands.steelPathFissureCommand);
   ctx
     .command("rjfissure", "当前九重天虚空裂隙")
     .alias("九重天裂缝")
     .alias("九重天裂隙")
-    .action(railjackFissureCommand);
+    .action(commands.railjackFissureCommand);
 
   ctx.command("weekly", "周常任务").alias("周常").action(inDevelopment);
   ctx
     .command("circuit", "本周回廊战甲及灵化之源")
     .alias("灵化之源")
     .alias("灵化")
-    .action(circuitCommand);
+    .action(commands.circuitCommand);
   ctx.command("lichc", "c系玄骸武器").action(inDevelopment);
   ctx.command("lichi", "i系玄骸武器").action(inDevelopment);
 
-  ctx.command("about", "关于").action(aboutCommand);
-}
+  ctx.command("about", "关于").action(commands.aboutCommand);
+};
 
 const inDevelopment = () => {
   return "Work in progress...";
