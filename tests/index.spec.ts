83c316b872a4a3d4f6e21d07eef9d81f8dcdbfc3
Cloudea <cloudeasoft@qq.com>
2025-12-23 12:56:38 +0800

    chore(test): fix timeout error (#13)
    * chore(test): fix timeout error
    
    * fix(test): use scoped timeout

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,8 +1,8 @@
-import { Context } from "koishi";
+import { App } from "koishi";
 import mock from "@koishijs/plugin-mock";
 import * as warframe from "../src/index";
 
-const app = new Context();
+const app = new App();
 app.plugin(mock as any);
 app.plugin(warframe);
 
@@ -16,10 +16,14 @@ app.middleware(({ content }, next) => {
   }
 });
 
-before(() => app.start());
+describe("main", function () {
+  this.timeout(10000);
 
-it("example", async () => {
-  await client.shouldReply("天王盖地虎", "宝塔镇河妖");
-  await client.shouldReply("天王盖地虎");
-  await client.shouldNotReply("宫廷玉液酒");
+  before(() => app.start());
+
+  it("example", async () => {
+    await client.shouldReply("天王盖地虎", "宝塔镇河妖");
+    await client.shouldReply("天王盖地虎");
+    await client.shouldNotReply("宫廷玉液酒");
+  });
 });
