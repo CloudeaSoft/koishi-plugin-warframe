e07cbfd733f1414003c1c874f56e4009c91466d7
CloudeaSoft <cloudeasoft@qq.com>
2026-07-28 13:17:10 +0800

    fix(wf): hide inapplicable relic prices

--- src/components/wf.tsx
+++ src/components/wf.tsx
@@ -885,40 +885,44 @@ export function RelicComponent(relic: OutputRelic): Element {
               display:flex;
               gap:10px;`}
         >
-          <span
-            style="
-                color: var(--wf-platinum);
-                display:flex;
-                line-height:1;"
-          >
-            {item.platinum ?? '??'}
-            <svg
-              viewBox="0 0 18 18"
+          {item.platinum !== null && (
+            <span
               style="
+                  color: var(--wf-platinum);
+                  display:flex;
+                  line-height:1;"
+            >
+              {item.platinum ?? '??'}
+              <svg
+                viewBox="0 0 18 18"
+                style="
+                    color: rgb(64 64 64 / 75%);
+                    height: 1em;
+                    width: 1em;
+                    vertical-align: -.125em;
+                    fill: currentcolor;
+                    margin-left:2px;"
+              >
+                <use href="#icon-platinum"></use>
+              </svg>
+            </span>
+          )}
+          {item.ducats !== null && (
+            <span style="color: var(--wf-ducats);display:flex;line-height:1;">
+              {item.ducats ?? '??'}
+              <svg
+                viewBox="0 0 18 18"
+                style="
                   color: rgb(64 64 64 / 75%);
                   height: 1em;
                   width: 1em;
                   vertical-align: -.125em;
-                  fill: currentcolor;
-                  margin-left:2px;"
-            >
-              <use href="#icon-platinum"></use>
-            </svg>
-          </span>
-          <span style="color: var(--wf-ducats);display:flex;line-height:1;">
-            {item.ducats ?? '??'}
-            <svg
-              viewBox="0 0 18 18"
-              style="
-                color: rgb(64 64 64 / 75%);
-                height: 1em;
-                width: 1em;
-                vertical-align: -.125em;
-                fill: currentcolor;"
-            >
-              <use href="#icon-ducats"></use>
-            </svg>
-          </span>
+                  fill: currentcolor;"
+              >
+                <use href="#icon-ducats"></use>
+              </svg>
+            </span>
+          )}
           <span>
             {relicRewardDropRate[item.rarity]}
             %
